85ffdbd4276259eb89734f6e267ba07ee65bd3da
Shivang <32.shivangdubey@gmail.com>
2021-07-27 09:20:32 +0530

    Initialized vite,vue,vuex,vue-router and sass.

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Count is {{counter}}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const counter: Ref<number> = ref(0);
+
+    function increment() {
+      counter.value++;
+    }
+
+    return { counter, increment };
+  },
+});
+</script>
+
+<style scoped>
+</style>
