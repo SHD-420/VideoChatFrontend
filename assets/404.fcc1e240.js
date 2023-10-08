2053b95474bfc1863abc91f50669d16f57e8db77
SHD-420 <32.shivangdubey@gmail.com>
2023-10-08 17:33:07 +0530

    Deploy

---
+++ assets/404.fcc1e240.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r,a as c}from"./index.69386d5f.js";const t={},s=c("h1",null,"404 error",-1),_=[s];function a(n,l){return o(),r("div",null,_)}var f=e(t,[["render",a]]);export{f as default};
