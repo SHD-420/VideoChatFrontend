fb4ccd7430860b8b01866aaa0a038f4e9a4dd856
SHD-420 <32.shivangdubey@gmail.com>
2023-10-08 13:07:53 +0530

    build: change deploy to https instead of git in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git init
 git add -A
 git commit -m "Deploy"
 
-git push -f git@github.com:SHD-420/VideoChatFrontend master:deploy
+git push -f https://github.com/SHD-420/VideoChatFrontend.git main:deploy
 
 cd ..
 rm -rf dist
